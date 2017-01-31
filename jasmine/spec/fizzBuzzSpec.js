0f2781f9d644f0d795aeaec3f39979dbc27383d2
Shereen Finney <shereenfinney@gmail.com>
2017-01-31 13:56:02 +0000

    Added some more tests in Fizzbuzz

--- jasmine/spec/fizzBuzzSpec.js
+++ jasmine/spec/fizzBuzzSpec.js
@@ -3,4 +3,14 @@ describe("FizzBuzz", function() {
   it("returns fizz when given a number divisible by 3", function() {
     expect(fizzbuzz(6)).toEqual("Fizz");
   });
+
+  it("returns buzz when given a number divisible by 5", function() {
+    expect(fizzbuzz(10)).toEqual("Buzz");
+  });
+
+  it("returns fizzbuzz when given a number divisible by 3 and 5", function(){
+    expect(fizzbuzz(15)).toEqual("FizzBuzz")
+  });
+
+
 });
--- jasmine/src/FizzBuzz.js
+++ jasmine/src/FizzBuzz.js
@@ -1,5 +1,14 @@
 
 fizzbuzz = function(number) {
+
+  if(number % 5 == 0 && number % 3 == 0){
+    return "FizzBuzz";
+  }
+
   if(number % 3 == 0){
   return "Fizz";}
+
+  if(number % 5 == 0){
+    return "Buzz";
+  }
 }
